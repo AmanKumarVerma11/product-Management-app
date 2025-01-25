c3e08ffa4fd085e850a8d8a9b626043b5eead3e4
Aman Kumar Verma <akverma11aug2002@gmail.com>
2025-01-25 20:13:40 +0530

    implement dynamic CORS configuration to allow specific origins and methods

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,18 @@ const allowedOrigins = [
   'http://localhost:5173'
 ];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Enhanced MongoDB Connection
